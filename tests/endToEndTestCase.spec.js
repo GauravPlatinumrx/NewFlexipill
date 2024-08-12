cf7e3f3e1dac6e51d6056b1ffc4597964255749d
Gaurav Sharma <sankhyan.gaurav2000@gmail.com>
2024-08-12 13:53:13 +0530

    fixes scroll issue

--- tests/endToEndTestCase.spec.js
+++ tests/endToEndTestCase.spec.js
@@ -130,10 +130,13 @@ test('Test 1', async ({ page }) => {
       await page.waitForTimeout(1000)
       await page.getByRole('link', { name: 'Profile' }).click();
       await page.click("(//div[@class='my-orders_order_details__info_cont__qAMR5'])[1]");      
-      await page.waitForTimeout(1000)
-      const cancelOrderButton = await page.$("//button[normalize-space()='Cancel Order']"); 
       await page.waitForTimeout(2000)
-      cancelOrderButton.scrollIntoViewIfNeeded();
+
+      await page.evaluate(() => window.scrollBy(0, 400))
+      await page.waitForTimeout(2000)
+      const cancelOrderButton = await page.$("//button[normalize-space()='Cancel Order']"); 
+      // await page.waitForTimeout(2000)
+      // cancelOrderButton.scrollIntoViewIfNeeded();
       await page.waitForTimeout(2000)
       cancelOrderButton.click()
     await page.waitForTimeout(1000);
